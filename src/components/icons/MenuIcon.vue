b8ea46103c4748e2bc47d3ec4cfcbbf75292d788
Just-Hosam <hosam_firas@hotmail.com>
2025-01-27 20:38:03 -0700

    Icons

---
+++ src/components/icons/MenuIcon.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+defineProps({
+  color: {
+    type: String,
+    default: "#5D5FEF",
+  },
+  size: {
+    type: Number,
+    default: 18,
+  },
+  strokeWidth: {
+    type: Number,
+    default: 2,
+  },
+  fill: {
+    type: String,
+    default: "#5D5FEF",
+  },
+});
+</script>
+
+<template>
+  <svg
+    :width="size"
+    :height="size"
+    :fill="fill"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M21 7V5H3.023V7H21ZM3 11H21V13H3V11ZM3 17H21V19H3V17Z"
+      :stroke="color"
+      :stroke-width="strokeWidth"
+    />
+  </svg>
+</template>

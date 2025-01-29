969ef145146e06a3b2cd3a7a98bdf52188f9404f
Just-Hosam <hosam_firas@hotmail.com>
2025-01-29 10:18:05 -0700

    Icons

---
+++ src/components/icons/ArrowUpIcon.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+defineProps({
+  color: {
+    type: String,
+    default: "var(--color-text-primary)",
+  },
+  size: {
+    type: Number,
+    default: 24,
+  },
+  strokeWidth: {
+    type: Number,
+    default: 1.5,
+  },
+  fill: {
+    type: String,
+    default: "transparent",
+  },
+});
+</script>
+
+<template>
+  <svg
+    :width="size"
+    :height="size"
+    :fill="fill"
+    :stroke="color"
+    :stroke-width="strokeWidth"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="lucide lucide-arrow-up"
+  >
+    <path d="m5 12 7-7 7 7" />
+    <path d="M12 19V5" />
+  </svg>
+</template>
---
+++ src/components/icons/SortIcon.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+defineProps({
+  color: {
+    type: String,
+    default: "var(--color-text-primary)",
+  },
+  size: {
+    type: Number,
+    default: 24,
+  },
+  strokeWidth: {
+    type: Number,
+    default: 1.5,
+  },
+  fill: {
+    type: String,
+    default: "transparent",
+  },
+});
+</script>
+
+<template>
+  <svg
+    :width="size"
+    :height="size"
+    :fill="fill"
+    :stroke="color"
+    :stroke-width="strokeWidth"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="lucide lucide-arrow-down-up"
+  >
+    <path d="m3 16 4 4 4-4" />
+    <path d="M7 20V4" />
+    <path d="m21 8-4-4-4 4" />
+    <path d="M17 4v16" />
+  </svg>
+</template>
